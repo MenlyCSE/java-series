1028aca9da5a456eaa7e6062df94a69710b85e96
MenlyCSE <MenlyCSE@gmail.com>
2024-08-06 04:28:55 -0400

    update

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
+import java.awt.*;
+import java.util.Date;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Point point1 = new Point(1, 1);
+        Point point2 = point1;
+        point1.x = 2;
+        System.out.println(point2);
     }
 }
