5e8dd09690d76c95ed40c231af43ba76b0e20f58
MenlyCSE <MenlyCSE@gmail.com>
2024-08-06 06:06:01 -0400

    update

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,8 @@
-import java.awt.*;
-import java.util.Date;
-
 public class Main {
     public static void main(String[] args) {
-        Point point1 = new Point(1, 1);
-        Point point2 = point1;
-        point1.x = 2;
-        System.out.println(point2);
+//      // arrays
+        int[] numbers = new int[5];
+        numbers[0] = 1;
+        numbers[1] = 2;
     }
 }
