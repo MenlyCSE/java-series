2a15d1def36b18ff7a793eac8b8c341895506a98
MenlyCSE <MenlyCSE@gmail.com>
2024-08-07 00:11:56 -0400

    update

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-//      // arrays
-        int[] numbers = new int[5];
-        numbers[0] = 1;
-        numbers[1] = 2;
+//      // vars
+        double x = 1.1;
+        double y = x + 2;
+        System.out.println(y);
     }
 }
