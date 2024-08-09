ad81c685c890388367638662fd55010468e31ecd
MenlyCSE <MenlyCSE@gmail.com>
2024-08-09 13:47:52 -0400

    update

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,13 @@
+import java.text.NumberFormat;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-//      // vars
-        double x = 1.1;
-        double y = x + 2;
-        System.out.println(y);
+        // ternary operator
+        int income = 120_000;
+        String className = income > 100_000 ? "First" : "Economy";
+
+        // output
+        System.out.println(className);
     }
 }
