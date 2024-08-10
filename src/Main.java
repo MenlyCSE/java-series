e7cd81fe0c55e91793f884c60049f8c6fd35427a
MenlyCSE <MenlyCSE@gmail.com>
2024-08-10 08:32:42 -0400

    update

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,11 @@
-import java.text.NumberFormat;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner userInput = new Scanner(System.in);
 
-        // user input objects
-        System.out.print("Number: ");
-        int number = userInput.nextInt();
-
-        Boolean isDivisibleBy3 = number % 3 == 0;
-        Boolean isDivisibleBy5 = number % 5 == 0;
-
-        // conditions
-
-
+        // for each
+        String[] fruits = {"Apple", "Mango", "Orange", "Strawberries"};
+        for (String fruit : fruits)
+            System.out.println(fruit);
     }
 }
