4f290f9214b2c785cc70942596b6e0ce2dd9fa57
MenlyCSE <MenlyCSE@gmail.com>
2024-08-09 20:16:22 -0400

    update

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,17 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        // ternary operator
-        int income = 120_000;
-        String className = income > 100_000 ? "First" : "Economy";
+        Scanner userInput = new Scanner(System.in);
 
-        // output
-        System.out.println(className);
+        // user input objects
+        System.out.print("Number: ");
+        int number = userInput.nextInt();
+
+        Boolean isDivisibleBy3 = number % 3 == 0;
+        Boolean isDivisibleBy5 = number % 5 == 0;
+
+        // conditions
+
+
     }
 }
